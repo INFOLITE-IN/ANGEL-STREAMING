8190a759bbf63eaca9cc69ce49451bc9de5521a3
SOCIAL MECHANIC <95063433+SOCIAL-MECHANIC-1997@users.noreply.github.com>
2022-07-02 22:37:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/SOCIAL-MECHANIC-1997/ANGEL-STREAMING /ANGEL-STREAMING
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/SOCIAL-MECHANIC-1997/ANGEL-STREAMING -b $BRANCH /ANGEL-STREAMING
 fi
 cd /ANGEL-VEDIO-STREAM
 pip3 install -U -r requirements.txt
